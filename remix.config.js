3cd65a40e7ee6f661bee023e9ea870d525ad933d
Xen <1435589+xendarboh@users.noreply.github.com>
2022-07-03 01:31:00 +0000

    build: install package nanoid
    Add nanoid to remix serverDependenciesToBundle as an ESM-only package

--- remix.config.js
+++ remix.config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   ignoredRouteFiles: [".*"],
+  serverDependenciesToBundle: [/^nanoid.*/],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
