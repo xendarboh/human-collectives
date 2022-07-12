0641deb9ab5c2549e83ea5dba13404a0b57f7add
Xen <1435589+xendarboh@users.noreply.github.com>
2022-07-12 19:20:32 +0000

    fix: Alert components JSX attributes

--- app/ui/alerts.tsx
+++ app/ui/alerts.tsx
@@ -8,9 +8,9 @@ const AlertSuccess = ({ children }: AlertProps) => (
         viewBox="0 0 24 24"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
           d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
@@ -31,9 +31,9 @@ const AlertError = ({ children }: AlertProps) => (
         viewBox="0 0 24 24"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
           d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
