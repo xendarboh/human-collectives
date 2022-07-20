127f14970fb9a8e6fd76ac7780d4472067920e82
Xen <1435589+xendarboh@users.noreply.github.com>
2022-07-20 06:13:18 +0000

    wip: change hardhat typechain dir output and target

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
 };
 
 export default config;
