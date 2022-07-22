edeab776ff60d6b4333a76c9b623396046631051
Xen <1435589+xendarboh@users.noreply.github.com>
2022-07-22 05:27:08 +0000

    wip: install some packages directly that have imports, ignore coverage directory

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
