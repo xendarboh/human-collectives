8f7d57fda0cde53023c81d2f12f5b46bbb667c0c
Xen <1435589+xendarboh@users.noreply.github.com>
2022-07-26 17:05:36 +0000

    feat: display humanode auth error messages on login page

--- app/routes/callback.tsx
+++ app/routes/callback.tsx
@@ -1,8 +1,15 @@
 // TODO: move to app/routes/auth/humanode/callback
-import { authenticator } from "~/auth.server";
 import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 
-export let loader: LoaderFunction = ({ request }) => {
+import { authenticator } from "~/auth.server";
+
+export let loader: LoaderFunction = async ({ request }) => {
+  // if errors in the callback url, forward them to be shown on the login page
+  const url = new URL(request.url);
+  const error = url.searchParams.get("error");
+  if (error) return redirect(`/login${url.search}`);
+
   return authenticator.authenticate("humanode", request, {
     successRedirect: "/dashboard",
     failureRedirect: "/login",
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,6 +3,7 @@ import { Form, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
 
 import type { AuthenticatedUser } from "~/auth.server";
+import { AlertError } from "~/ui/alerts";
 import { authenticator } from "~/auth.server";
 import { commitSession } from "~/session.server";
 import { getSession } from "~/session.server";
@@ -14,8 +15,26 @@ type LoaderData = {
 
 export const loader: LoaderFunction = async ({ request }) => {
   const auth = await authenticator.isAuthenticated(request);
+
+  // check for an error in the session "flash" message
   const session = await getSession(request.headers.get("Cookie"));
   let error = session.get(authenticator.sessionErrorKey);
+
+  // check for the error in the URL, as returned by humanode Oauth2, for example:
+  // {
+  //   error: "login request denied",
+  //   error_description: "You canceled the authentication process",
+  //   state: "22222222-eeee-4444-bbbb-888888888888",
+  // }
+  if (!error) {
+    const url = new URL(request.url);
+    const uError = [
+      url.searchParams.get("error"),
+      url.searchParams.get("error_description"),
+    ];
+    if (uError[0]) error = { message: uError.join(": ") };
+  }
+
   return json(
     { auth, error },
     {
@@ -30,11 +49,11 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function Login() {
   const data = useLoaderData() as LoaderData;
   return (
-    <div className="mt-8 grid place-items-center">
+    <div className="mt-8 grid place-items-center gap-8">
+      {data.error && <AlertError>{data.error?.message}</AlertError>}
       <Form action="/auth/humanode" method="post">
         <button className="btn btn-primary">Authenticate with Humanode</button>
       </Form>
-      <div>{data.error && <p>ERROR: {data.error?.message}</p>}</div>
     </div>
   );
 }
